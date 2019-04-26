7a3ec3d4b48907d73d51a238d8666385cbafae9b
haotian <ritou11@gmail.com>
2019-04-26 14:12:43 +0800

    init cli

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hub-cup
+
+go 1.12
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
+	github.com/juju/loggo v0.0.0-20190212223446-d976af380377 // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "os"
+  //"fmt"
+  cli "gopkg.in/urfave/cli.v1"
+  "github.com/juju/loggo"
+)
+
+func main() {
+  app := &cli.App{
+		Name:      "hub-sync",
+		UsageText: `hub-sync <what> [<from>]`,
+		Usage:     "Make your github forks catch up with origins",
+		Version:   "0.1.0",
+		HideHelp:  true,
+		Flags: []cli.Flag{
+      &cli.StringFlag{
+        Name: "token, t",
+        Usage: "Github token, see https://github.com/settings/tokens",
+      },
+			&cli.StringFlag{
+        Name: "token-file",
+        Usage: "`path` to your Github token file",
+        Value: "~/.hub-cup",
+      },
+      &cli.BoolFlag{Name: "force, f", Usage: "As if {git push --force}"},
+      &cli.BoolFlag{Name: "dry-run, n", Usage: "Don't actually update"},
+			&cli.BoolFlag{Name: "debug", Usage: "print debug messages"},
+			&cli.BoolFlag{Name: "help, h", Usage: "print the help"},
+		},
+		Action:  func (c *cli.Context) error {
+      if c.Bool("help") || len(c.Args()) == 0 {
+        cli.ShowAppHelpAndExit(c, 0)
+      }
+      if c.Bool("debug") {
+    		loggo.ConfigureLoggers("<root>=DEBUG;")
+    	} else {
+    		loggo.ConfigureLoggers("<root>=INFO;")
+    	}
+      what := c.Args()[0]
+      from := ""
+      if len(c.Args()) > 1 {
+        from = c.Args()[1]
+      }
+      return nil
+    },
+		Authors: []cli.Author{{Name: "Nogeek", Email: "ritou11@gmail.com"}},
+	}
+
+	app.Run(os.Args)
+}
